9fd1892f4bc83f8fd7b3aa12aab9226e642a2a18
AnkitaHota-05 <hotaankita8@gmail.com>
2022-03-19 20:10:25 +0530

    initialcomment

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>re</div>
+    /* <Phone /> */
+    /* <OTP /> */
+    /* <PersonalDetails /> */
+    /* <BankDetails /> */
+  )
+}
+
+export default Register
